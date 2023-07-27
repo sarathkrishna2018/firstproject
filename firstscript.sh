3bccfeb6ba819dab76e7603ebc55bba685e0529e
sarathkrishna2018 <39905871+sarathkrishna2018@users.noreply.github.com>
2023-07-27 21:08:40 +0530

    added 10th line

--- firstscript.sh
+++ firstscript.sh
@@ -7,3 +7,4 @@ echo "The original fourthline"
 echo "This is the sixth line"
 connect_to_database("database_production","admin","password")
 echo "this is the ninth line"
+echo "this is the tenth line"
