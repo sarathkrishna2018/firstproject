d611799f665c7095fa2c51d38eceee39cada19a3
sarathkrishna2018 <39905871+sarathkrishna2018@users.noreply.github.com>
2023-07-27 21:18:14 +0530

    i changed the third line for so and so reason

--- firstscript.sh
+++ firstscript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "This is the first line"
 echo " This will be the second line"
-echo "this is the third line"
+echo "this is the third line modified"
 echo "Created the fourthline"
 echo "The original fourthline"
 echo "This is the sixth line"
